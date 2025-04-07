710e9990521e8abffb434243786f77da437be6b1
HollyRiver <hcssk2800@gmail.com>
2025-04-07 17:50:07 +0900

    .

--- RP2025/Practice/250331_ch5_tidyverse 통합 패키지.R
+++ RP2025/Practice/250331_ch5_tidyverse 통합 패키지.R
@@ -58,7 +58,7 @@ tibble(
 ## 변수 확장성
 mytbl <- tibble(
   x = 1:5,
-  y = 1,
+  y = 1,  # recycling rule
   z = x^2 + y,
   `1st` = 11:15,
   `2nd` = 21:25,
@@ -67,6 +67,31 @@ mytbl <- tibble(
 
 mytbl
 
+## 실제 테이블에 들어가는 가시적 순서대로 데이터를 입력할 수 있는 모듈도 존재 -> 안씀
+
+
 ## 상호 변환
 iris %>% as_tibble
 flights %>% as.data.frame
+
+
+mytbl <- tibble(
+  x = 1:5,
+  y = 1,  # recycling rule
+  z = x^2 + y,
+)
+
+mydf <- data.frame(mytbl)
+
+class(mydf[, 1:2])  ## df
+class(mydf[, 1])  ## vector
+
+class(mytbl[, 1:2]) ## tbl
+class(mytbl[, 1])   ## tbl. 동일한 자료구조를 가짐
+
+mytbl[, 1]
+
+## 이렇게 해야 벡터 추출이 가능
+mytbl[[1]]
+mytbl$x
+mytbl[["x"]]
---
+++ RP2025/Practice/250407_ch6_데이터 랭글링.r
@@ -0,0 +1,114 @@
+## Date : 2025-04-07
+## Author : 강신성
+## Student code : 202014107
+## Title : 데이터 랭글링
+
+#----------1. readr 패키지-----------
+library(tidyverse)
+setwd("C:/Users/default.DESKTOP-VHFHFGU/Downloads") ## 세션에서 설정하면 이걸 쓰는듯
+## RStudio를 아이콘으로 열면 작업공간이 디폴트로 변경
+## 이전에 사용했던 R script 파일로 열면 기존 작업공간 유지
+
+data = read_csv("C:/Users/default.DESKTOP-VHFHFGU/Downloads/StudentSurvey.csv")
+
+read_csv("StudentSurvey.csv", skip = 5) ## n개의 열을 무시함
+read_csv("StudentSurvey.csv", col_names = FALSE, na = "9999") ## 열 이름 X1~Xn
+
+
+#----------2. dplyr 패키지-----------
+## 행 관련 - flights 데이터셋
+library(nycflights13)
+flights  ## 마지막 열 : <dttm> -> date time
+
+dim(flights) ## (336776, 19)
+
+flights %>% head  ## tibble같은 경우에는 그냥 보는 게 많음
+flights %>% str   ## .info() 느낌
+flights %>% glimpse  ## dplyr 함수. 일견. str과 유사하나, 콘솔 크기에 맞춤
+
+### filter()
+#### 120분 이상 연착한 항공편 - arr_delay가 120분 이상인 샘플
+flights %>% filter(arr_delay >= 120)
+
+#### 1월 1일에 출발한 항공편 - month는 1, day는 1인 샘플
+flights %>% filter((month == 1) & (day == 1)) ## 832개
+flights %>% filter(month == 1, day == 1) ## 되도록 만들어짐
+
+#### 3, 5, 8월에 출발한 항공편
+flights %>% filter(month %in% c(3, 5, 8))
+
+value = 3
+value %in% c(3, 5, 8)
+
+
+### 행 관련 - arrange()
+#### 출발 시간 순으로 정렬
+flights %>% arrange(year, month, day, dep_time) ## 그냥 여러 변수 넣어도 됨
+
+#### 가장 출발 지연시간이 길었던 항공편 확인
+flights %>% arrange(desc(dep_delay))
+
+flights %>% arrange(desc(arr_delay))
+
+#### 대략 정시에 출발한 항공편 중에서(|"실제 출발 시간" - "예정된 출발 시간"| <= 10분)
+#### 도착 지연시간이 가장 길었던 항공편 확인
+flights %>% arrange(dep_delay)  ## ??? 일찍 출발한 경우도 존재함
+
+flights %>%
+  filter(dep_delay >= -10 & dep_delay <= 10) %>%
+  arrange(desc(arr_delay)) %>%
+  .[1, ]
+
+### 열 관련 - select() : 데이터베이스 생각하면 됨
+flights %>% select(year, month, day)  ## 몇 개 변수만 선택
+
+flights %>% glimpse  ## year행부터 day행까지 추출하려면?
+
+flights %>% select(year:day)  ## 벡터 만들듯이
+
+#### 특정 변수 제외 선택
+flights %>% select(-year)
+flights %>% select(-year, -day)
+flights %>% select(-(year:day))
+
+#### 추출 시 변수 이름 변경까지 됨
+flights %>%
+  select(dep.time = dep_time,
+         arr_time = arr_time)
+
+
+### select 도우미 함수
+flights %>% select(starts_with("sched")) ## 변수 이름이 "sched"로 시작하는 경우
+flights %>% select(ends_with("time"))    ## 변수 이름이 "time"으로 끝나는 경우
+
+flights %>% select(contains("arr"))         ## 변수 이름에 "arr"이 포함되는 경우
+## -> carrier라는 의도하지 않은 변수가 들어감
+flights %>% select(contains("arr_"))
+
+
+
+### 열 관련 - mutate -> 다음 시간에
+
+
+
+### 열 관련 - rename : 특정 변수 이름 변경
+flights %>% rename(dep.time = dep_time,
+                   arr.time = arr_time) %>%
+  glimpse  ## select와 동일한 사용법. 하지만 얘는 이름만 바꿔줌.
+
+# dt %>% rename(cnt2025 = `2025count`) ## 이런 식으로 씀
+
+
+### 열 관련 - relocate : 변수의 위치 변경
+flights %>%
+  filter(dep_delay <= 10 & dep_delay >= -10) %>%
+  arrange(desc(arr_delay)) %>%
+  relocate(arr_delay, .after = day) ## 앞에 day를 둠
+
+flights %>%
+  filter(dep_delay <= 10 & dep_delay >= -10) %>%
+  arrange(desc(arr_delay)) %>%
+  relocate(arr_delay, .before = day) ## 뒤에 day를 둠
+
+flights %>% relocate(air_time, distance) ## 기본적으로 앞에 데려옴
+flights %>% relocate(carrier:tailnum, .before = day)  ## 콜론 사용 문법은 공유
